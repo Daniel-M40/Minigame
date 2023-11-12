34e98411809dd602a0e9ef4f6774328f3380c1b5
Daniel-M40 <daniel.moir0993@gmail.com>
2023-11-12 15:40:12 +0000

    Implemented timer for time trials

--- Source/Minigame/GameModes/TimeTrailsGameMode.cpp
+++ Source/Minigame/GameModes/TimeTrailsGameMode.cpp
@@ -2,3 +2,16 @@
 
 
 #include "TimeTrailsGameMode.h"
+
+void ATimeTrailsGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorldTimerManager().SetTimer(
+		TimerHandle, this, &ATimeTrailsGameMode::IncreaseTimer, 1.f, true, 0.0);
+}
+
+void ATimeTrailsGameMode::IncreaseTimer()
+{
+	Timer++;
+}
--- Source/Minigame/GameModes/TimeTrailsGameMode.h
+++ Source/Minigame/GameModes/TimeTrailsGameMode.h
@@ -13,4 +13,25 @@ UCLASS()
 class MINIGAME_API ATimeTrailsGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+	#pragma region Properties
+	
+private:
+	FTimerHandle TimerHandle;
+
+public:
+	UPROPERTY(BlueprintReadOnly, Category="Time")
+	float Timer = 0.f;
+
+	
+	#pragma endregion
+
+	#pragma region Actions
+
+	void BeginPlay() override;
+	
+	void IncreaseTimer();
+	
+	#pragma endregion
+	
 };
