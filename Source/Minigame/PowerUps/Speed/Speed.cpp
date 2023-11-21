34892856ad93df7b7dc8f244a5bb87547ef54179
Daniel-M40 <daniel.moir0993@gmail.com>
2023-11-21 18:00:54 +0000

    Created speed power up - increase tanks speed by a value and for a specific time when overlapped

---
+++ Source/Minigame/PowerUps/Speed/Speed.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Speed.h"
+
+#include "Minigame/Pawns/Tank/Tank.h"
+
+
+// Sets default values
+ASpeed::ASpeed()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ASpeed::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASpeed::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ASpeed::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//Get functionality from parent class
+	Super::OnOverlapBegin(OverlappedComp, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+	
+	UE_LOG(LogTemp, Warning, TEXT("Speed Overlap"));
+	
+	
+	//Apply power up to tank
+	if (Tank)
+	{
+		Tank->EnableFasterMovement(Multiplier, SpeedIncreaseDuration);
+	}
+	
+	//Destroy power up after pickup
+	Destroy();
+}
+
---
+++ Source/Minigame/PowerUps/Speed/Speed.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Minigame/PowerUps/PowerUp.h"
+#include "Speed.generated.h"
+
+UCLASS()
+class MINIGAME_API ASpeed : public APowerUp
+{
+	GENERATED_BODY()
+
+#pragma region Properties
+	
+private:
+	UPROPERTY(EditAnywhere, Category="Config")
+	float SpeedIncreaseDuration = 5.f;
+
+	UPROPERTY(EditAnywhere, Category="Config")
+	float Multiplier = 5.f;
+	
+#pragma endregion
+
+#pragma region Properties
+	
+public:
+	// Sets default values for this actor's properties
+	ASpeed();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+#pragma endregion
+};
